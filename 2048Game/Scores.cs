abededdd48597bb43e4ca759dc1b5095ea1b8e92
Tattao <shihantao5@hotmail.com>
2015-05-21 18:29:32 +0800

    20150521P

---
+++ 2048Game/Scores.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace _2048Game
+{
+    public class Scores : INotifyPropertyChanged
+    {
+        private int score;
+        public int Score
+        {
+            get { return score; }
+            set
+            {
+                if (value != score)
+                {
+                    score = value;
+                    RaisePropertyChanged("Score");
+                }
+            }
+        }
+
+        public Scores()
+        {
+            Score = 0;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+}
